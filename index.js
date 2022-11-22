394549276752c700b787009689e4fc32a10edc84
mermaidProgrammer <117957651+mermaidProgrammer@users.noreply.github.com>
2022-11-21 20:57:22 -0600

    Condensed Function Call Statements

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //set-up a global variable
-let farenheitTemp = 25;
+let farenheitTemp = null;
 
 //Challenge - Get today's date
 function formatDate(cDate) {
@@ -46,51 +46,58 @@ function formatDate(cDate) {
   return `Updated: ${hour}:${minString}, ${day}, ${monthShort} ${date}, ${year}`;
 }
 
+//Challenge 4 - Update current Temperature for given city
+function showTemperature(response) {
+  console.log(response.data);
+  //update city name
+  let newCity = document.querySelector("h1");
+  newCity.innerHTML = `Weather for: ${response.data.name}`;
+  //update temperture
+  let temperatureElement = document.querySelector("#num-temp");
+  let roundTemp = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = `${roundTemp}`;
+  farenheitTemp = roundTemp;
+  //update condition
+  let condElement = document.querySelector("#c-cond");
+  condElement.innerHTML = `${response.data.weather[0].main}`;
+  //update windspeed
+  let windSpeedElement = document.querySelector("#w-speed");
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}`;
+  //update the current weather icon
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  //update the alternate text in the current weather icon
+  iconElement.setAttribute("alt", `${response.data.weather[0].main}`);
+}
+
+//Challenge 2- Use a form to update the city name
+function search(city) {
+  let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  //  Call - update the current temperature
+  axios.get(apiURL).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let newCityInput = document.querySelector("#city-input");
+  let city = newCityInput.value;
+  search(city);
+}
+
 //Report out current Date
 let todaysDate = document.querySelector("#current-date");
 todaysDate.innerHTML = formatDate(new Date());
 
-//Challenge 2- Use a form to update the city name,
-function updateCity(event) {
-  event.preventDefault();
-  let newCityInput = document.querySelector("#city-input");
-  let city = newCityInput.value;
+//Call upon loading page
+search("Chicago");
 
-  //Challenge 4 - Update current Temperature for given city
-  let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-
-  function showTemperature(response) {
-    console.log(response.data);
-    //update city name
-    let newCity = document.querySelector("h1");
-    newCity.innerHTML = `Weather for: ${response.data.name}`;
-    //update temperture
-    let temperatureElement = document.querySelector("#num-temp");
-    let roundTemp = Math.round(response.data.main.temp);
-    temperatureElement.innerHTML = `${roundTemp}`;
-    farenheitTemp = roundTemp;
-    //update condition
-    let condElement = document.querySelector("#c-cond");
-    condElement.innerHTML = `${response.data.weather[0].main}`;
-    //update windspeed
-    let windSpeedElement = document.querySelector("#w-speed");
-    windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}`;
-    //update the current weather icon
-    let iconElement = document.querySelector("#icon");
-    iconElement.setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    //update the alternate text in the current weather icon
-    iconElement.setAttribute("alt", `${response.data.weather[0].main}`);
-  }
-  //  Call - update the current temperature
-  axios.get(apiURL).then(showTemperature);
-}
-//   Call - Use a form to update the city name
+//Call - Use a form to update the city name
 let searchNewCityForm = document.querySelector("#city-form");
-searchNewCityForm.addEventListener("submit", updateCity);
+searchNewCityForm.addEventListener("submit", handleSubmit);
 
 //Challenge 3 - Switch between Farenheit and Celcius
 function changeC(event) {
